99f7ee55f8a972e4af7af08c91b1fc2ac16c4c6e
Andy Huang <cerberus@ayxh.dev>
2024-07-03 16:48:39 +0000

    First commit

---
+++ src/test/java/com/corndel/framerate/FramerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corndel.framerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
