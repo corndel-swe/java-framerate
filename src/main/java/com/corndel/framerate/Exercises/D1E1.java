b8820be9bedba8c85fbc078e74e37e8f6af83a8d
Andy Huang <cerberus@ayxh.dev>
2024-07-08 11:56:57 +0000

    Start adding exercises

---
+++ src/main/java/com/corndel/framerate/Exercises/D1E1.java
@@ -0,0 +1,20 @@
+package com.corndel.framerate.Exercises;
+
+import io.javalin.Javalin;
+
+import io.javalin.http.Context;
+
+public class D1E1 {
+  private Javalin app;
+
+  public D1E1() {
+    app = Javalin.create()
+        .get("/", StaticFileController::get);
+  }
+
+  static class StaticFileController {
+    public static void get(Context ctx) {
+
+    }
+  }
+}
