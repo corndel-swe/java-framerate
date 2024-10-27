a763f6869ad7985dd8f972f442674fc41e930cb9
Simon Haines <nimon@hotmail.co.uk>
2024-10-27 23:35:35 +0000

    relative paths

--- src/main/java/com/corndel/framerate/exercises/D2E3.java
+++ src/main/java/com/corndel/framerate/exercises/D2E3.java
@@ -1,5 +1,8 @@
 package com.corndel.framerate.exercises;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.template.JavalinThymeleaf;
@@ -9,7 +12,16 @@ public class D2E3 {
     var app = Javalin.create(
         config -> {
           config.staticFiles.add("/exercises/public", Location.CLASSPATH);
-          config.fileRenderer(new JavalinThymeleaf());
+
+          var resolver = new ClassLoaderTemplateResolver();
+          resolver.setPrefix("/exercises/templates/");
+          resolver.setSuffix(".html");
+          resolver.setTemplateMode("HTML");
+
+          var engine = new TemplateEngine();
+          engine.setTemplateResolver(resolver);
+
+          config.fileRenderer(new JavalinThymeleaf(engine));
         });
 
     app.get(
